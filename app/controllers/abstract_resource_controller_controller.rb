487713c255512507eea59acaa0f13eb54ac40fc3
Nakamura shinichirou <naka5313@gmail.com>
2010-06-26 20:39:52 +0900

    RESOURCE ADD

--- app/controllers/abstract_resource_controller_controller.rb
+++ app/controllers/abstract_resource_controller_controller.rb
@@ -10,4 +10,35 @@ class AbstractResourceControllerController < ApplicationController
       render :text => "Forbidden", :status => "405 not allowed"
     }
   end
+
+    private
+  def initLogFormat(logFileName)
+    logfile="./log/"+logFileName
+    progname="log"
+    formatter = Log4r::PatternFormatter.new(:pattern => "%d %C[%l]: %M",
+    :date_format => "%Y/%m/%d %H:%M:%S")
+
+    @logger = Log4r::Logger.new(progname)
+    outputter = Log4r::FileOutputter.new(
+              "file",
+    :filename => logfile,
+    :trunc => false,
+    :formatter => formatter)
+    @logger.add(outputter)
+  end
+
+  def logInfo(message)
+    initLogFormat("app_webservice_Info.log")
+    @logger.info(message)
+  end
+
+  def logError(message)
+    initLogFormat("app_webservicer_error.log")
+    @logger.error(message)
+  end
+
+  def logDebug(message)
+    initLogFormat("app_webservice_debug.log")
+    @logger.debug(message)
+  end
 end
