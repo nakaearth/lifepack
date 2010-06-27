d4f4dd6815da9fc5917c21723a67e862c54c5a03
Nakamura shinichirou <naka5313@gmail.com>
2010-06-27 13:10:41 +0900

    ROOT EDIT

--- app/controllers/service/service.rb
+++ app/controllers/service/service.rb
@@ -2,20 +2,20 @@
 # and open the template in the editor.
 
 module Service
-  class Task < ActionWebService::Struct
-    include ROXML
+ # class Task < ActionWebService::Struct
+ #   include ROXML
 
-    xml_attr :id
-    xml_attr :content
-    xml_attr :status
-    xml_attr :user_id
-    xml_attr :priority
+  #  xml_attr :id
+  #  xml_attr :content
+  #  xml_attr :status
+  #  xml_attr :user_id
+  #  xml_attr :priority
 
-    member :id, :integer
-    member :content, :string
-    member :status, :integer
-    member :user_id, :string
-    member :priority, :integer
-  end
+   # member :id, :integer
+   # member :content, :string
+   # member :status, :integer
+   # member :user_id, :string
+   # member :priority, :integer
+  #end
     
 end
